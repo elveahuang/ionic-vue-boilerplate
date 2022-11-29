5c0c96481f30329f7d35c14b2de8b87df31cf52c
elveahuang <elvea.huang@gmail.com>
2022-11-29 07:33:05 +0000

    Deploying to gh-pages from @ elveahuang/ionic-vue-capacitor-boilerplate@c14048e5d00d5c87f9fa073e588aa23c2f5878fd 🚀

--- assets/status-tap.d6f6fd20.js
+++ assets/status-tap.d6f6fd20.js
@@ -1,3 +1,3 @@
-import{r as a,l as i,m as c,q as d,s as l}from"./index.b9681d9e.js";/*!
+import{r as a,l as i,m as c,q as d,s as l}from"./index.c4709b1b.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
