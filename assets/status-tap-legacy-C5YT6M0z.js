3e74e8017bd79691c25986152aad7883c1a062e4
elveahuang <elvea.huang@gmail.com>
2024-03-23 07:02:46 +0000

    Deploying to gh-pages from @ elveahuang/ionic-vue-boilerplate@159ac264acb6d14f9c39f31e5e137509d313e329 🚀

--- assets/status-tap-legacy-C5YT6M0z.js
+++ assets/status-tap-legacy-C5YT6M0z.js
@@ -1,5 +1,5 @@
-System.register(["./index-legacy-yOfrUowu.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.q,r=e.s,s=e.t,o=e.v,i=e.x}],execute:function(){
+System.register(["./index-legacy-Bmy33UHa.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.q,r=e.s,s=e.t,o=e.v,i=e.x}],execute:function(){
 /*!
-       * (C) Ionic http://ionicframework.com - MIT License
-       */
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
 e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise((e=>s(c,e))).then((()=>{o((async()=>{c.style.setProperty("--overflow","hidden"),await i(c,300),c.style.removeProperty("--overflow")}))}))}))}))}))}}}));
